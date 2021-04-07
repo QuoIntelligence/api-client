dd1bac2413ca887639978c8131f29bea58d3be35
Mohamed <mohamed.abdel-maksoud@quointelligence.eu>
2021-04-07 17:10:56 +0200

    Add minimal setup.py to get pip to work

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quointelligence',
+      version='0-alpha',
+      description='Python client for Quointelligence API',
+      url='https://github.com/QuoIntelligence/api-client/tree/master/python',
+      author='Quointelligence GmbH',
+      author_email='info@quointelligence.eu',
+      license='Apache 2',
+      packages=['quointelligence'],
+      zip_safe=False)
